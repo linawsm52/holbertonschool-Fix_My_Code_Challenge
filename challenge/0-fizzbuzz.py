ecf12027619f926de3db0efe749fc8deec1f7145
linawsm52 <lenaexo0@gmail.com>
2026-05-05 02:44:16 +0300

    new file

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -2,16 +2,17 @@
 import sys
 
 def fizzbuzz(n):
+    result = []
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
+            result.append("FizzBuzz")
         elif i % 3 == 0:
-            print("Fizz", end=" ")
+            result.append("Fizz")
         elif i % 5 == 0:
-            print("Buzz", end=" ")
+            result.append("Buzz")
         else:
-            print(i, end=" ")
-    print()
+            result.append(str(i))
+    print(" ".join(result))
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
